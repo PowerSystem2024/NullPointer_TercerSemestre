dece75c44af4a6b0e3b3994c8eca818c9b719f5d
Horacio Quiles <quileshoxar@gmail.com>
2025-05-28 22:09:49 -0300

    agrego juego piedra papel y tijera

---
+++ Tecnicatura 3er Sem/JavaScript/Juegos/Piedra Papel Tijera/codigo.js
@@ -0,0 +1,52 @@
+function randomNum(min, max) {
+    return Math.floor(Math.random()* (max - min + 1)) + min
+}
+// 1 sera piedra 2 sera papel 3 sera tijera
+
+function eleccion(jugada){
+    let resultado = ""
+    if(jugada == 1){
+        resultado ="Piedra 🌑"
+    }else if(jugada == 2){
+        resultado ="Papel 🧻"
+    }else if(jugada == 3){
+        resultado ="Tijera ✂"
+    }else {
+        resultado = "Mal elegido"
+    }
+    return resultado
+}
+
+let jugador = 0
+let pc = 0
+let triunfos = 0
+let perdidas = 0
+//jugador = prompt("Elige : 1 piedra, 2 papel, 3 tijera")
+
+while(triunfos < 3 && perdidas < 3){
+    pc = aleatorio(1, 3)
+    jugador = prompt("Elige : 1 piedra, 2 papel, 3 tijera")
+    //alert("Elige jugador"+jugador)
+
+alert("PC elige: "+eleccion(pc))
+alert("Tu eliges: "+eleccion(jugador))
+
+//Combate
+if(pc == jugador){
+    alert("EMPATE!!!")
+} else if(jugador == 1 && pc == 3){
+    alert("GANASTE!!!")
+    triunfos = triunfos + 1
+} else if(jugador == 2 && pc == 1){
+    alert("GANASTE!!!")
+    triunfos = triunfos + 1
+} else if(jugador == 3 && pc == 2){
+    alert("GANASTE!!!")
+    triunfos = triunfos + 1
+} else {
+    alert("PERDISTE!!!")
+    perdidas = perdidas + 1
+}
+}
+
+alert("GANASTE "+triunfos+" veces. Perdiste "+perdidas+" veces.")
