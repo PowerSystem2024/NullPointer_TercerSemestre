ed284cbdd821534d134b3ada805ee4adf3027ac1
Horacio Quiles <quileshoxar@gmail.com>
2025-06-04 20:15:51 -0300

    agrego clase 5 juego Avatar

--- Tecnicatura 3er Sem/JavaScript/Juegos/Avatar/public/js/avatar.js
+++ Tecnicatura 3er Sem/JavaScript/Juegos/Avatar/public/js/avatar.js
@@ -1,6 +1,16 @@
+let ataqueJugador
+let ataqueEnemigo
+
 function iniciarJuego(){
     let botonPersonajeJugador = document.getElementById('boton-personaje');
     botonPersonajeJugador.addEventListener('click', seleccionarPersonajeJugador);
+
+    let botonPunio = document.getElementById('boton-punio') 
+    botonPunio.addEventListener('click', ataquePunio)
+    let botonPatada = document.getElementById('boton-patada')
+    botonPatada.addEventListener('click', ataquePatada)
+    let botonBarrida = document.getElementById('boton-barrida')
+    botonBarrida.addEventListener('click', ataqueBarrida)
 }
 
 function seleccionarPersonajeJugador(){
@@ -25,14 +35,46 @@ function seleccionarPersonajeJugador(){
     }
     
     seleccionarPersonajeEnemigo();
+}
 
-    function seleccionarPersonajeEnemigo() {
+function seleccionarPersonajeEnemigo() {
     let personajes = ['Zuko', 'Katara', 'Aang', 'Toph'];
     let indiceAleatorio = Math.floor(Math.random() * personajes.length);
 
     let spanPersonajeEnemigo = document.getElementById('personaje-enemigo');
     spanPersonajeEnemigo.innerHTML = personajes[indiceAleatorio]; 
+    }
+
+function ataquePunio(){ //Modificamos la variable global ataqueJugador
+    ataqueJugador = 'Punio'
+    ataqueAleatorioEnemigo()
 }
 
+function ataquePatada(){ //Modificamos la variable global ataqueJugador
+    ataqueJugador = 'Patada'
+    ataqueAleatorioEnemigo()
 }
+
+function ataqueBarrida(){ //Modificamos la variable global ataqueJugador
+    ataqueJugador = 'Barrida'
+    ataqueAleatorioEnemigo()
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+    
+}
+
+function ataqueAleatorioEnemigo(){//Ahora ocupando la variable global nueva le decimos el ataque y necesitamos la funcion aleatoria nuevamente
+    let ataqueAleatorio = aleatorio(1, 3)
+
+    if(ataqueAleatorio == 1){
+        ataqueEnemigo = 'Punio'
+    } else if(ataqueAleatorio == 2){
+        ataqueEnemigo = 'Patada'
+    } else {
+        ataqueEnemigo = 'Barrida'
+    }
+}
+
 window.addEventListener('load', iniciarJuego)
