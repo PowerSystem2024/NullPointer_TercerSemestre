8dd35836443db482beb0f3153e30c4fdcd4ec9d2
GimenezCristian <gimenezc779@gmail.com>
2025-06-09 00:10:58 -0300

    Agrego la leccion 8

---
+++ Tecnicatura 3er Sem/Java/Leccion08/ManejoExcepciones/src/Aritmetica.java
@@ -0,0 +1,9 @@
+public class Aritmetica {
+    public static int division(int numerador, int denominador)
+    {
+        if (denominador == 0) {
+            throw new OperacionExcepcion("División entre cero");
+        }
+        return numerador / denominador;
+    }
+}
---
+++ Tecnicatura 3er Sem/Java/Leccion08/ManejoExcepciones/src/TestExcepciones.java
@@ -0,0 +1,20 @@
+public class TestExcepciones {
+    public static void main(String[] args) {
+        int resultado = 0;
+        try {
+            resultado = Aritmetica.division(10, 0);
+        } catch (OperacionExcepcion e) {
+            System.out.println("Ocurrió un error de tipo OperacionExcepcion");
+            System.out.println(e.getMessage());
+        }catch (Exception e){
+            System.out.println("Ocurrió un error");
+            e.printStackTrace(System.out);
+           System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Se revisó la división por cero");
+        }
+
+        System.out.println("La variable de resultado tiene como valor: " + resultado);
+    }
+}
